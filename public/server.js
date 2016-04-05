d442d558062bab5fe20174520190dfb5f6ed8f03
Giap Tran <giaptd@gmail.com>
2016-04-05 17:42:48 +0700

    commit

---
+++ public/server.js
@@ -0,0 +1,15 @@
+﻿var app = require('express').createServer();
+var io = require('socket.io')(app);
+
+app.listen(88);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
